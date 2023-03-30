16e57b4426ca6788b0eb111bdb3e159862b0b452
Brian Le <83758821+brianle169@users.noreply.github.com>
2023-03-31 00:36:42 +0700

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const words = string.split("");
+  let result = "";
+  for (let i = words.length - 1; i >= 0; i--) {
+    result += words[i];
+  }
+  return result;
 };
 
 // Do not edit below this line
