1fdcc60eb56144a562464546e782be35ba79500d
Brian Le <83758821+brianle169@users.noreply.github.com>
2023-03-31 02:03:31 +0700

    Update better solution from ToP

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,9 +1,5 @@
 const leapYears = function (year) {
-  if (year % 4 == 0 && year % 100 != 0) {
-    return true;
-  } else if (year % 100 == 0 && year % 400 == 0) {
-    return true;
-  } else return false;
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
 
 console.log(leapYears(1900));
