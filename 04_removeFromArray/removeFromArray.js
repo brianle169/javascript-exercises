c9d9598228a5aad872c848f52efeefb1ed3a841a
Brian Le <83758821+brianle169@users.noreply.github.com>
2023-03-31 01:22:11 +0700

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  //arr is arguments[0]
+  const args = Array.from(arguments);
+  const origin = args[0];
+  const beReplaced = args.slice(1);
 
+  for (const item of beReplaced) {
+    inner: for (const x of origin) {
+      if (x === item) {
+        origin.splice(origin.indexOf(x), 1);
+        break inner;
+      }
+    }
+  }
+  return origin;
 };
+
+removeFromArray([1, 2, 3, 4], 5, 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
