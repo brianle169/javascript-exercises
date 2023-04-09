1abe801c604da34f34d154b7cc4ba14385bbad3e
Brian Le <83758821+brianle169@users.noreply.github.com>
2023-04-09 16:26:37 +0700

    Finished 11_getTheTitles with two solutions

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,15 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (arr) {
+   return arr.reduce((array, item) => {
+      array.push(item.title);
+      return array;
+   }, []);
 };
+
+//TOP solution: map() returns a deep copy of the argument arr, which can be better
+/*
+const getTheTitles = function (arr) {
+    return arr.map(item => item.title);
+}*/
 
 // Do not edit below this line
 module.exports = getTheTitles;
