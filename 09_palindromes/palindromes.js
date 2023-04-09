e8dfc70d77d8edf6f03721d64ac31bcc40a99cbb
Brian Le <83758821+brianle169@users.noreply.github.com>
2023-04-09 16:00:54 +0700

    Finished 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+// const palindromes = function (string) {
+//    const regex = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+//    const original = string;
+//    const processedArray = string
+//       .toLowerCase()
+//       .split("")
+//       .filter((char) => regex.indexOf(char) === -1)
+//       .join("");
+//    return (
+//       processedArray ===
+//       original
+//          .toLowerCase()
+//          .split("")
+//          .filter((char) => regex.indexOf(char) === -1)
+//          .join("")
+//    );
+// };
 
+const palindromes = function (string) {
+   const processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+   return processedString.split("").reverse().join("") === processedString;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
