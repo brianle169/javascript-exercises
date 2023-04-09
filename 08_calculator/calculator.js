8cf076c5ca9575612f54e19e4d858ca7d6475669
Brian Le <83758821+brianle169@users.noreply.github.com>
2023-04-09 14:57:48 +0700

    Finished 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,37 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+   return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+   return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+   if (nums.length === 0) return 0;
+   else return nums.reduce((total, num) => (total += num), 0);
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+   if (nums.length === 0) return 0;
+   else return nums.reduce((total, num) => (total *= num));
 };
 
-const power = function() {
-	
+const power = function (num, pow) {
+   if (pow === 0) return 1;
+   return num * power(num, pow - 1);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+   if (num === 0) return 1;
+   return num * factorial(num - 1);
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+   add,
+   subtract,
+   sum,
+   multiply,
+   power,
+   factorial,
 };
