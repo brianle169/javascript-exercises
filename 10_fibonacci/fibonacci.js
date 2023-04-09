d183fa6468911ed8b4dd332337fca4b43d3a6893
Brian Le <83758821+brianle169@users.noreply.github.com>
2023-04-09 16:19:16 +0700

    Finished 10_fibonacci, added recursive answer but this one is not efficient time-complexity wise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function (val) {
+   let defaultVal = 1;
+   if (val <= 0) return "OOPS";
+   else if (val == 1 || val == 2) return defaultVal;
+   else {
+      return fibonacci(val - 1) + fibonacci(val - 2);
+   }
 };
 
 // Do not edit below this line
