7aa5150173186aeace7a784bfa21093b5647cefc
Brian Le <83758821+brianle169@users.noreply.github.com>
2023-04-09 17:08:36 +0700

    Finished 12_findTheOldest, failed initially and had to install some other packages

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+   const now = new Date().getFullYear();
+   const result = people.sort((p1, p2) => {
+      if (!p1.yearOfDeath || !p2.yearOfDeath) {
+         const age1 = now - p1.yearOfBirth;
+         const age2 = now - p2.yearOfBirth;
+         return age2 - age1;
+      }
+      return (
+         p2.yearOfDeath - p2.yearOfBirth - (p1.yearOfDeath - p1.yearOfBirth)
+      );
+   })[0];
+   return result;
 };
 
 // Do not edit below this line
